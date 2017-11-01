4099c8a7a5de61ff120a3ecb1024659f96378e2e
Cristian Ramirez <cristian.ramirez019@gmail.com>
2017-11-01 09:54:13 -0600

    Initial commit

---
+++ Ejercicio1.php
@@ -0,0 +1,15 @@
+<h2>Ejercicio 1</h2>
+<?php
+/*Crear dos variables cuyo nombre sea "uno" y "dos"
+he imprímelas por pantalla. Pon un comentario  con 
+el tipo de dato que contiene.
+
+*/
+
+// string
+$uno = "abc";
+// integer
+$dos = 123;
+echo "Uno: ".$uno."<br/>";
+echo "Dos: ".$dos."<hr/>";
+?>
